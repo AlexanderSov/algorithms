d90daa4098ff605c4181724c241d8491bcaabb3e
AlexanderSov <aleksandr.plaxin@gmail.com>
2019-05-05 21:54:05 +0300

    Add merge sort

---
+++ algorithms/sort/merge_sort.py
@@ -0,0 +1,32 @@
+def mergeSort(alist):
+    """
+    Complexity: O(n*log n)
+    Memory: additional for 2 sublist
+    """
+    print("Splitting ", alist)
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i, j, k = 0, 0, 0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k] = lefthalf[i]
+                i += 1
+            else:
+                alist[k] = righthalf[j]
+                j += 1
+            k += 1
+
+        while i < len(lefthalf):
+            alist[k] = lefthalf[i]
+            i += 1
+            k += 1
+
+        while j < len(righthalf):
+            alist[k] = righthalf[j]
+            j += 1
+            k += 1
